41610c1fa684be2cfdfb07fdff53dd66c6befb62
dima <agodimon@mail.ru>
2021-06-05 14:54:18 +0500

    Делаю ДЗ №3. выполнил ввод с клавиатуры цифр в textView.

--- app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.android_homework_lesson_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.txt);
+    }
+    @SuppressLint("SetTextI18n")
+    public void appendText(View view){
+        Button button=(Button) view;
+    txt.setText(txt.getText().toString()+button.getText().toString());
     }
 }
