bbcdba0fe101aaa22343fae9702b00d8ee6a5479
dima <agodimon@mail.ru>
2021-06-06 20:55:37 +0500

    Выполнил ДЗ №3. 1. Напишите обработку каждой кнопки из макета калькулятора. 2. Создайте объект с данными и операциями калькулятора. Продумайте, каким образом будете хранить введённые пользователем данные. 3. * Создайте макет калькулятора для горизонтальной ориентации экрана и отображайте его в ландшафтной ориентации.

--- app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
@@ -1,58 +1,177 @@
 package com.example.android_homework_lesson_calculator;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-public class MainActivity extends AppCompatActivity {
-    TextView txt;
-    double buffer;
-    char op;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements Serializable {
+
+    private static final String KEY_VALUE = "key_value";
+    private static final String KEY_OPERATOR = "key_operator";
+    private static final String KEY_RESULT = "key_result";
+    private Button btnNumberOne;
+    private Button btnNumberTwo;
+    private Button btnNumberThree;
+    private Button btnNumberFour;
+    private Button btnNumberFive;
+    private Button btnNumberSix;
+    private Button btnNumberSeven;
+    private Button btnNumberEight;
+    private Button btnNumberNine;
+    private Button btnNumberDot;
+    private Button btnNumberZero;
+    private TextView txtResult;
+    private double valueBuffer;
+    private char operator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        op='1';
-        buffer=0;
-        txt=findViewById(R.id.txt);
-        Button clearNum=findViewById(R.id.clear);
-        clearNum.setOnClickListener(view->{
-            buffer=0;
-            txt.setText("");
+        operator = '1';
+        valueBuffer = 0;
+        txtResult = findViewById(R.id.txt);
+        Button clearNum = findViewById(R.id.clear);
+
+        initButton();
+
+        clearNum.setOnClickListener(view -> {
+            valueBuffer = 0;
+            txtResult.setText("");
         });
-        Button ans=findViewById(R.id.button_equals);
-        ans.setOnClickListener(view->{
-            switch (op){
+        Button ans = findViewById(R.id.button_equals);
+        ans.setOnClickListener(view -> {
+            switch (operator) {
                 case '+':
-                    buffer+=Double.parseDouble(txt.getText().toString());
-                    txt.setText(String.valueOf(buffer));
+                    valueBuffer += Double.parseDouble(txtResult.getText().toString());
+                    txtResult.setText(String.valueOf(valueBuffer));
                     break;
                 case '-':
-                    buffer-=Double.parseDouble(txt.getText().toString());
-                    txt.setText(String.valueOf(buffer));
+                    valueBuffer -= Double.parseDouble(txtResult.getText().toString());
+                    txtResult.setText(String.valueOf(valueBuffer));
                     break;
                 case '/':
-                    buffer/=Double.parseDouble(txt.getText().toString());
-                    txt.setText(String.valueOf(buffer));
+                    valueBuffer /= Double.parseDouble(txtResult.getText().toString());
+                    txtResult.setText(String.valueOf(valueBuffer));
                     break;
                 case '*':
-                    buffer*=Double.parseDouble(txt.getText().toString());
-                    txt.setText(String.valueOf(buffer));
+                    valueBuffer *= Double.parseDouble(txtResult.getText().toString());
+                    txtResult.setText(String.valueOf(valueBuffer));
                     break;
             }
+
+            operator = 0;
         });
     }
-    @SuppressLint("SetTextI18n")
-    public void appendText(View view){
-        Button button=(Button) view;
-    txt.setText(txt.getText().toString()+button.getText().toString());
+
+    private void initButton() {
+        btnNumberOne = findViewById(R.id.button1);
+        btnNumberOne.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 1;
+            txtResult.setText(value);
+        });
+
+        btnNumberTwo = findViewById(R.id.button2);
+        btnNumberTwo.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 2;
+            txtResult.setText(value);
+        });
+
+        btnNumberThree = findViewById(R.id.button3);
+        btnNumberThree.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 3;
+            txtResult.setText(value);
+        });
+
+        btnNumberFour = findViewById(R.id.button4);
+        btnNumberFour.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 4;
+            txtResult.setText(value);
+        });
+
+        btnNumberFive = findViewById(R.id.button5);
+        btnNumberFive.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 5;
+            txtResult.setText(value);
+        });
+
+        btnNumberSix = findViewById(R.id.button6);
+        btnNumberSix.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 6;
+            txtResult.setText(value);
+        });
+
+        btnNumberSeven = findViewById(R.id.button7);
+        btnNumberSeven.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 7;
+            txtResult.setText(value);
+        });
+
+        btnNumberEight = findViewById(R.id.button8);
+        btnNumberEight.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 8;
+            txtResult.setText(value);
+        });
+
+        btnNumberNine = findViewById(R.id.button9);
+        btnNumberNine.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 9;
+            txtResult.setText(value);
+        });
+
+        btnNumberDot = findViewById(R.id.button_point);
+        btnNumberDot.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + ".";
+            txtResult.setText(value);
+        });
+
+        btnNumberZero = findViewById(R.id.button0);
+        btnNumberZero.setOnClickListener(v -> {
+            String value = txtResult.getText().toString() + 0;
+            txtResult.setText(value);
+        });
+
     }
-    public void ops(View view){
-    buffer=Double.parseDouble(txt.getText().toString());
-        Button button=(Button) view;
-        op= button.getText().charAt(0);
-        txt.setText("");
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle instanceState) {
+        super.onSaveInstanceState(instanceState);
+        instanceState.putDouble(KEY_VALUE, valueBuffer);
+        instanceState.putString(KEY_RESULT, txtResult.getText().toString());
+        instanceState.putChar(KEY_OPERATOR, operator);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        double value = instanceState.getDouble(KEY_VALUE);
+        String result = instanceState.getString(KEY_RESULT);
+        char operator = instanceState.getChar(KEY_OPERATOR);
+        setConfig(value, result, operator);
+    }
+
+    private void setConfig(double value, String result, char operator) {
+        valueBuffer = value;
+        if (operator != 0) {
+            this.operator = operator;
+        }
+
+        txtResult.setText(result);
+    }
+
+
+
+    public void ops(View view) {
+        valueBuffer = Double.parseDouble(txtResult.getText().toString());
+        Button button = (Button) view;
+        operator = button.getText().charAt(0);
+        txtResult.setText("");
     }
 }
