44a437902f522077b4ec752f54331b5a7ed7cd97
dima <agodimon@mail.ru>
2021-06-08 20:13:06 +0500

    делаю переключение тем. в MainActivity прописал переключение радиокнопок.

--- app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
+++ app/src/main/java/com/example/android_homework_lesson_calculator/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.android_homework_lesson_calculator;
 
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 import java.io.Serializable;
 
@@ -18,6 +23,13 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     private static final String KEY_OPERATOR = "key_operator";
     private static final String KEY_RESULT = "key_result";
 
+    private static final String NameSharedPreference = "LOGIN";
+    // Имя параметра в настройках
+    private static final String AppTheme = "APP_THEME";
+    private static final int MyCoolCodeStyle = 0;
+    private static final int AppThemeLightCodeStyle = 1;
+    private static final int AppThemeCodeStyle = 2;
+
 
     private TextView txtResult;
     private double valueBuffer;
@@ -26,14 +38,72 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getAppTheme(R.style.dark_theme));
         setContentView(R.layout.activity_main);
         initButton();
         operator = '1';
         valueBuffer = 0;
         txtResult = findViewById(R.id.txt);
+        initThemeChooser();
 
 
+    }
 
+    private int getAppTheme(int codeStyle) {
+        return codeStyleToStyleId(getCodeStyle(codeStyle));
+    }
+
+    // Инициализация радиокнопок
+    private void initThemeChooser() {
+        initRadioButton(findViewById(R.id.radio_btn_dark_theme), MyCoolCodeStyle);
+        initRadioButton(findViewById(R.id.radio_btn_orange_theme),AppThemeLightCodeStyle);
+        initRadioButton(findViewById(R.id.radio_btn_light_theme), AppThemeCodeStyle);
+        RadioGroup rg = findViewById(R.id.radioButtons);
+        ((MaterialRadioButton) rg.getChildAt(getCodeStyle(MyCoolCodeStyle))).setChecked(true);
+    }
+
+    // Все инициализации кнопок очень похожи, поэтому создадим метод для
+    //переиспользования
+    private void initRadioButton(View button, final int codeStyle) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// сохраним настройки
+                setAppTheme(codeStyle);
+// пересоздадим активити, чтобы тема применилась
+                recreate();
+            }
+        });
+    }
+
+    // Сохранение настроек
+    private void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+                MODE_PRIVATE);
+// Настройки сохраняются посредством специального класса editor.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(AppTheme, codeStyle);
+        editor.apply();
+    }
+
+    private int codeStyleToStyleId(int codeStyle) {
+        switch (codeStyle) {
+            case AppThemeCodeStyle:
+                return R.style.orange_theme;
+            case AppThemeLightCodeStyle:
+                return R.style.light_theme;
+            default:
+                return R.style.dark_theme;
+        }
+    }
+
+    // Чтение настроек, параметр «тема»
+    private int getCodeStyle(int codeStyle) {
+// Работаем через специальный класс сохранения и чтения настроек
+        SharedPreferences sharedPref = getSharedPreferences(NameSharedPreference,
+                MODE_PRIVATE);
+//Прочитать тему, если настройка не найдена - взять по умолчанию
+        return sharedPref.getInt(AppTheme, codeStyle);
     }
 
     private void initButton() {
